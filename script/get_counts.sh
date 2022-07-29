b26df38279f1ba00da7c938039625b8cef5eb866
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2022-07-29 10:32:41 +0100

    Update get_counts.sh

--- script/get_counts.sh
+++ script/get_counts.sh
@@ -22,19 +22,21 @@ already_count(){
             check_sra # if entry is sra file check if converted correctly
     fi
 }
+
+
 dowload_sample () {
 	echo "Starting dowloading experiment $1, sample $2 " >&3
 	type=$(check_filetype ${2})
 	echo "${2} is a $type" >&3
-
-	if [[ "$(find .)" =~ "counts_filtered" ]] # Check if final output already exist and skip 1 loop
-    then
-        printf "Sample already pre-processed!\n"
-        cd ../../
-    elif [[ "$(find .)" =~ "fastq" ]] # check if fastq files are already present
-    then
-        already_count $type
-    else
+	if [[ "${filetype}" == "BAM" ]]
+        then
+        	#echo "Downloading BAM"
+            download_bam # if entry is bam file check if converted correctly
+        else
+        	#echo "Downloading SRA"
+        	download_sra ${2}
+            check_sra # if entry is sra file check if converted correctly
+    fi
         
 	# fasterq-dump SRR12021928.sra -o SRR12021928 -O fastq -Svp |& tee -a test_fasterq-dump.LOG
   	# commands
@@ -47,8 +49,11 @@ pwd
 while IFS= read -r sample; do
 	echo "Processing sample $sample" >&3
 	mkdir -p ../Experiments/$experiment
-	mkdir -p ../Experiments/$experiment/$samples_names
-	cd ../Experiments/$experiment/$samples_names
+	mkdir -p ../Experiments/$experiment/$sample
+	cd ../Experiments/$experiment/$sample
 	dowload_sample $experiment $sample
+	get_counts
+	
+
 done < $samples_names
 
