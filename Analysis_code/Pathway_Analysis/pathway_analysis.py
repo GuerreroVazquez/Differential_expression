cdca414ce10881266759cdf816f422aeb7835287
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2022-12-05 16:49:45 +0000

    Update pathway_analysis.py

--- Analysis_code/Pathway_Analysis/pathway_analysis.py
+++ Analysis_code/Pathway_Analysis/pathway_analysis.py
@@ -16,6 +16,11 @@ databases_names = gseapy.get_library_name()
 
 
 def get_pathways(genes_file='../../Results/def_expressed.csv'):
+    """
+    From the gene_files csv, gets ALL the pathways in ALL the databases of ALL the experiments
+    :param genes_file: CSV file with the experiments df genes in each column.
+    :return:
+    """
     df, names = a_g.get_data(all_experiments_de=genes_file)
     df_results = {}
     df = a_g.remove_version(genes_data_frame=df, experiment_names=names, output=None)
@@ -40,22 +45,31 @@ def get_pathways(genes_file='../../Results/def_expressed.csv'):
     return df_results
 
 
-def get_pathways(genes_file='../../Results/def_expressed_gene_names.csv'):
+def get_pathways(genes_file='../../Results/def_expressed_gene_names.csv', sel_databases_names=None):
+    """
+     From the gene_files csv, gets the pathways in  the databases in databases_names
+      of ALL the experiments
+     :param sel_databases_names: List of string with the subset of gseapy.get_library_name()
+     :param genes_file: CSV file with the experiments df genes in each column.
+     :return:
+     """
+    if sel_databases_names is None:
+        sel_databases_names = ['GO_Molecular_Function_2021',
+                               'GO_Cellular_Component_2021',
+                               'GO_Biological_Process_2021',
+                               'Reactome_2022',
+                               'KEGG_2021_Human']
     df, names = a_g.get_data(all_experiments_de=genes_file)
     df_results = pd.DataFrame()
     df = a_g.remove_version(genes_data_frame=df, experiment_names=names, output=None)
 
-    databases_names = ['GO_Molecular_Function_2021',
-                       'GO_Cellular_Component_2021',
-                       'GO_Biological_Process_2021',
-                       'Reactome_2022',
-                       'KEGG_2021_Human']
+    sel_databases_names
 
     for experiment in names:
         DEGs = df[experiment].tolist()
         DEGs = [x for x in DEGs if pd.notnull(x)]
         experiment_dic = pd.DataFrame()
-        for database in databases_names:
+        for database in sel_databases_names:
             try:
                 enr_GOBP_up = gp.enrichr(gene_list=DEGs,
                                          gene_sets=[database],
@@ -108,6 +122,7 @@ def get_wordcloud(df, title):
     plt.savefig(f"Wordcloud_{title}")
     return wordcloud.words_
 
+
 def test_get_pathways():
     """
 
@@ -122,16 +137,22 @@ def test_get_pathways():
     print(result)
 
 
-def chicharron():
+def chicharron(diff_expression=default_pathway_file, pvalue_threshold=0.1, apvlue_threshold=1, ratio_treshold=0):
     """
-    This funtion will get the csv of the patways generated previously and ... we will see
+    This function will get the csv of the pathways generated previously and get the patways that
+    have a criteria of pvalue, adjusted pvalue and ratio
+    :param apvlue_threshold: float Threshold that marks up to which value to take the adjusted P value
+    :param pvalue_threshold:  float Threshold that marks up to which value to take the P value
+    :param ratio_treshold: float What is the minimun ratio (genes found/ genes total in pathway)
+    :param diff_expression: str the CSV file that holds the output of gseapy
+
     :return:
     """
-    df = pd.read_csv(default_pathway_file)
-    pvalue_threshold = 0.1
-    apvlue_threshold = 0.1
+    df = pd.read_csv(diff_expression)
+
     ratio = [int(x.split('/')[0]) / int(x.split('/')[1]) for x in df['Overlap']]
     df['Ratio'] = ratio
+    df = df.query(f"Pvalue < {pvalue_threshold}")
     df = df.query(f"Pvalue < {pvalue_threshold}")
     return df
 
